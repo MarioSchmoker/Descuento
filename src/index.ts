4da27172f8026b3ff0dd3439919cf01db2821df6
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-05-01 13:47:35 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let Precio: number = 450.5;
+let Descuento: number = 0.1;
+let Porcentaje: number = Precio * Descuento;
+let Resultado: number = Precio - Porcentaje;
+
+console.log("El precio es $", Precio);
+console.log("Descuento es: ", Descuento * 100, "%");
+console.log("El precio final es $", Resultado);
